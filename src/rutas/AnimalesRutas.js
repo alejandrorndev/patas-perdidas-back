15e2ba924e38608cbdd8c3833f39fe8e149b2685
Alejandro restrepo <alejandrorn.dev@gmail.com>
2024-09-29 00:03:20 -0500

    fix:se organiza la documentacion de esta ruta para registrar los animales

--- src/rutas/AnimalesRutas.js
+++ src/rutas/AnimalesRutas.js
@@ -22,7 +22,7 @@ const storage = multer.diskStorage({
  * /api/animales/registrar-animal:
  *   post:
  *     summary: Registrar un animal perdido
- *     description: Permite a un usuario autenticado registrar un animal perdido, proporcionando la información del animal y asociándolo al usuario autenticado.
+ *     description: Permite a un usuario autenticado registrar un animal perdido, proporcionando la información del animal, la dirección y las imágenes asociadas.
  *     tags:
  *       - Animales
  *     security:
@@ -30,7 +30,7 @@ const storage = multer.diskStorage({
  *     requestBody:
  *       required: true
  *       content:
- *         application/json:
+ *         multipart/form-data:
  *           schema:
  *             type: object
  *             required:
@@ -40,23 +40,25 @@ const storage = multer.diskStorage({
  *               - color
  *               - descripcion
  *               - fecha_perdida
+ *               - direccion
+ *               - imagenes
  *             properties:
  *               nombre_animal:
  *                 type: string
  *                 description: El nombre del animal.
- *                 example: "Max"
+ *                 example: "Lola"
  *               especie:
  *                 type: string
  *                 description: La especie del animal.
- *                 example: "Perro"
+ *                 example: "Gato"
  *               raza:
  *                 type: string
  *                 description: La raza del animal.
- *                 example: "Golden Retriever"
+ *                 example: "Criollo"
  *               color:
  *                 type: string
  *                 description: El color del animal.
- *                 example: "Dorado"
+ *                 example: "Blanco"
  *               descripcion:
  *                 type: string
  *                 description: Descripción adicional sobre el animal.
@@ -66,18 +68,24 @@ const storage = multer.diskStorage({
  *                 format: date
  *                 description: Fecha en la que el animal fue reportado como perdido.
  *                 example: "2024-09-26"
- *               estado:
+ *               direccion:
  *                 type: string
- *                 description: Estado actual del animal, por defecto 'perdido'.
- *                 example: "perdido"
+ *                 description: La dirección donde se perdió el animal.
+ *                 example: "calle 52 #20-05, El Vergel, Medellín"
+ *               imagenes:
+ *                 type: array
+ *                 items:
+ *                   type: string
+ *                   format: binary
+ *                 description: Lista de imágenes del animal.
  *             example:
- *               nombre_animal: "Max"
- *               especie: "Perro"
- *               raza: "Golden Retriever"
- *               color: "Dorado"
+ *               nombre_animal: "Lola"
+ *               especie: "Gato"
+ *               raza: "Criollo"
+ *               color: "Blanco"
  *               descripcion: "Lleva un collar rojo y tiene una cicatriz en la pata."
  *               fecha_perdida: "2024-09-26"
- *               estado: "perdido"
+ *               direccion: "calle 52 #20-05, El Vergel, Medellín"
  *     responses:
  *       200:
  *         description: Animal registrado exitosamente.
@@ -86,16 +94,59 @@ const storage = multer.diskStorage({
  *             schema:
  *               type: object
  *               properties:
- *                 message:
+ *                 status:
  *                   type: string
- *                   example: "Animal registrado exitosamente"
- *                 result:
+ *                   example: "OK"
+ *                 data:
  *                   type: object
- *                   description: Detalles de la operación en la base de datos.
+ *                   properties:
+ *                     id:
+ *                       type: integer
+ *                       example: 41
+ *                     especie:
+ *                       type: string
+ *                       example: "Gato"
+ *                     raza:
+ *                       type: string
+ *                       example: "Criollo"
+ *                     color:
+ *                       type: string
+ *                       example: "Blanco"
+ *                     descripcion:
+ *                       type: string
+ *                       example: "Lleva un collar rojo y tiene una cicatriz en la pata."
+ *                     fecha_perdida:
+ *                       type: string
+ *                       format: date
+ *                       example: "2024-09-26"
+ *                     direccion:
+ *                       type: string
+ *                       example: "calle 52 #20-05, El Vergel, Medellín"
+ *                     imagenes_subidas:
+ *                       type: array
+ *                       items:
+ *                         type: string
+ *                       example: [
+ *                         "1727585576256-pj11.png",
+ *                         "1727585576264-pj10.jpg",
+ *                         "1727585576269-WhatsApp Image 2024-09-15 at 6.28.28 PM.jpeg",
+ *                         "1727585576270-WhatsApp Image 2024-09-24 at 11.07.17 AM.jpeg"
+ *                       ]
+ *                     message:
+ *                       type: string
+ *                       example: "Animal registrado exitosamente"
  *       400:
  *         description: Error en los datos proporcionados o solicitud inválida.
  *       401:
- *         description: Token no proporcionado o no válido.
+ *         description: Token no proporcionado o inválido.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "Token no proporcionado o inválido"
  *       500:
  *         description: Error interno del servidor.
  */
